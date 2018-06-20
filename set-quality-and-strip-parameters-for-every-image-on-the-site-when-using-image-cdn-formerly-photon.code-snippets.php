6932baacbfb93bd1d3e8e32c8f5a5ce4d8eeddf8
Luke Cavanagh <lukecav@users.noreply.github.com>
2018-06-20 07:36:44 -0600

    Update set-quality-and-strip-parameters-for-every-image-on-the-site-when-using-image-cdn-formerly-photon.code-snippets.php
    Bump quality to 90 from 80.

--- set-quality-and-strip-parameters-for-every-image-on-the-site-when-using-image-cdn-formerly-photon.code-snippets.php
+++ set-quality-and-strip-parameters-for-every-image-on-the-site-when-using-image-cdn-formerly-photon.code-snippets.php
@@ -5,7 +5,7 @@
  */
 add_filter('jetpack_photon_pre_args', 'jetpackme_custom_photon_compression' );
 function jetpackme_custom_photon_compression( $args ) {
-    $args['quality'] = 80;
+    $args['quality'] = 90;
     $args['strip'] = 'all';
     return $args;
 }
