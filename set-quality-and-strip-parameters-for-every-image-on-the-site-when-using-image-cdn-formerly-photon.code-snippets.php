07414b6dae5384bfa83736c9c7174beec11cd7d4
Luke Cavanagh <lukecav@users.noreply.github.com>
2018-06-20 07:24:22 -0600

    Set quality and strip parameters for every image on the site when using Image CDN (formerly Photon) Add

---
+++ set-quality-and-strip-parameters-for-every-image-on-the-site-when-using-image-cdn-formerly-photon.code-snippets.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Set quality and strip parameters for every image on the site when using Image CDN (formerly Photon)
+ */
+add_filter('jetpack_photon_pre_args', 'jetpackme_custom_photon_compression' );
+function jetpackme_custom_photon_compression( $args ) {
+    $args['quality'] = 80;
+    $args['strip'] = 'all';
+    return $args;
+}
