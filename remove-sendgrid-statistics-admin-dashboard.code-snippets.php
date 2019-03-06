410714903422ff5f430019164ff9f6a2397d8cb7
Luke Cavanagh <lukecav@users.noreply.github.com>
2019-03-06 09:24:58 -0700

    Remove SendGrid Statistics Admin Dashboard Add

---
+++ remove-sendgrid-statistics-admin-dashboard.code-snippets.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Remove SendGrid Statistics Admin Dashboard
+ */
+add_action('plugins_loaded', function (): void {
+    remove_action('init', ['Sendgrid_Statistics', 'set_up_menu']);
+});
